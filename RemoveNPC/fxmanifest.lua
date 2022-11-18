7ab131fa9e93c69a3f9052de5e5bdf36014f0a91
Louchou <72306869+Louchou1@users.noreply.github.com>
2022-11-18 20:06:40 +0100

    Add files via upload

---
+++ RemoveNPC/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+
+client_script {
+    'removenpc_louchou.lua'
+}
+
+game 'gta5'
+author 'Louchou'
+description 'No NPC - By Louchou'
+version '1'
---
+++ RemoveNPC/removenpc_louchou.lua
@@ -0,0 +1,23 @@
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+            SetCreateRandomCops(false)
+            SetCreateRandomCopsNotOnScenarios(false)
+            SetCreateRandomCopsOnScenarios(false)
+            SetGarbageTrucks(false)
+            SetRandomBoats(false)
+            SetVehicleDensityMultiplierThisFrame(0)
+            SetPedDensityMultiplierThisFrame(0)
+            SetRandomVehicleDensityMultiplierThisFrame(0)
+            SetScenarioPedDensityMultiplierThisFrame(0, 0)
+            SetParkedVehicleDensityMultiplierThisFrame(0)
+
+            local x,y,z = table.unpack(GetEntityCoords(PlayerPedId()))
+            ClearAreaOfVehicles(x, y, z, 1000, false, false, false, false, false)
+            RemoveVehiclesFromGeneratorsInArea(x - 500.0, y - 500.0, z - 500.0, x + 500.0, y + 500.0, z + 500.0);
+
+            ClearAreaOfCops(x, y, z, 1000, 0)
+            ClearAreaOfPeds(x, y, z, 1000, 0)
+
+    end
+end)
